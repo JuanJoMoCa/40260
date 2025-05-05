f3bfbda607d0d6e4714e61be2697477eaa8bc8a1
JuanJoMoCa <J_Jose07Morales@outlook.es>
2025-05-05 11:59:09 -0600

    07-formulario

---
+++ html00/formulario.js
@@ -0,0 +1,14 @@
+console.log("inicio")
+
+const boton = document.getElementById("enviar")
+boton.addEventListener("click", 
+    () => {
+        console.log("los parametros")
+        let user=document.getElementById("user")
+        let password=document.getElementById("password")
+        console.log(password.value)
+        console.log(user.value)
+
+
+    }
+)
